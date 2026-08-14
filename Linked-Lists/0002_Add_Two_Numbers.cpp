c5a2230f069cf74cf3afec057b8858ad685ebc86
Deep1345 <pateldeepvi@gmail.com>
2026-08-14 21:38:32 +0530

    ✅ Add 2. Add Two Numbers [Medium]
    Folder : Linked-Lists/
    Tags   : Linked List, Math, Recursion
    Lang   : Cpp

--- Linked-Lists/0002_Add_Two_Numbers.cpp
+++ Linked-Lists/0002_Add_Two_Numbers.cpp
@@ -6,27 +6,27 @@
 // Date      : 2026-08-14
 //
 
-        ListNode dummy(-1);
-        ListNode* tail = &dummy;
-        int carry = 0;
-        while (l1 || l2 || carry) {
-            int sum = carry;
-            if (l1) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2) {
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-class Solution {
+class Solution {
 public:
- */
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode() : val(0), next(nullptr) {}
- *     int val;
- *     ListNode *next;
- * Definition for singly-linked list.
- * struct ListNode {
-                sum += l2->val;
-                l2 = l2->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1);
+        ListNode* tail = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
+            carry = sum / 10;
+        }
+        return dummy.next;
+
+    }
+};
