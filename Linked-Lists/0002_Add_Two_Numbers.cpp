b083ffff378508125a48992567e65786fac54443
Deep1345 <pateldeepvi@gmail.com>
2026-08-14 21:05:21 +0530

    ✅ Add 2. Add Two Numbers [Medium]
    Folder : Linked-Lists/
    Tags   : Linked List, Math, Recursion
    Lang   : Cpp

--- Linked-Lists/0002_Add_Two_Numbers.cpp
+++ Linked-Lists/0002_Add_Two_Numbers.cpp
@@ -16,16 +16,16 @@
                 l1 = l1->next;
             }
             if (l2) {
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 public:
-class Solution {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
  * };
  */
+class Solution {
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- *     ListNode() : val(0), next(nullptr) {}
- * struct ListNode {
- *     int val;
- *     ListNode *next;
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
