e44e56edcfa456158e06f8f8ba32be750f766231
Deep1345 <pateldeepvi@gmail.com>
2026-08-17 10:58:07 +0530

    ✅ Add 983. Validate Stack Sequences [Medium]
    Folder : Stacks/
    Tags   : Array, Stack, Simulation
    Lang   : Cpp

--- Stacks/0983_Validate_Stack_Sequences.cpp
+++ Stacks/0983_Validate_Stack_Sequences.cpp
@@ -9,30 +9,15 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int n = pushed.size();
-        int i = 0;
-        int j = 0;
         stack<int> st;
-        while(i<n){
-            while(!st.empty() && st.top()==popped[j]){
+        int j = 0;
+        for (int x : pushed) {
+            st.push(x);
+            while (!st.empty() && st.top() == popped[j]) {
                 st.pop();
                 j++;
             }
-            st.push(pushed[i]);
-            i++;  
         }
-        while(!st.empty()){
-            if(st.top()==popped[j]){
-                st.pop();
-                j++;
-            }
-            else{
-                break;
-            }
-        }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        return st.empty();
     }
 };
