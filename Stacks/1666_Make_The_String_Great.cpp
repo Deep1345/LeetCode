18932b1dae289766f20a83a1387dd0ef5a32646f
Deep1345 <pateldeepvi@gmail.com>
2026-08-18 22:21:36 +0530

    ✅ Add 1666. Make The String Great [Easy]
    Folder : Stacks/
    Tags   : String, Stack
    Lang   : Cpp

---
+++ Stacks/1666_Make_The_String_Great.cpp
@@ -0,0 +1,41 @@
+// Problem   : 1666. Make The String Great
+// Difficulty: Easy
+// Tags      : String, Stack
+// URL       : https://leetcode.com/problems/make-the-string-great/
+// Language  : Cpp
+// Date      : 2026-08-18
+//
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        for(char c: s){
+            if(st.empty()){
+                st.push(c);
+            }
+            else{
+                char prev = st.top();
+                if(islower(c)){
+                    if(c-'a'==prev-'A'){
+                        st.pop();
+                        continue;
+                    }
+                }
+                else{
+                    if(c-'A'==prev-'a'){
+                        st.pop();
+                        continue;
+                    }
+                }
+                st.push(c);
+            }
+        }
+        string ans;
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
