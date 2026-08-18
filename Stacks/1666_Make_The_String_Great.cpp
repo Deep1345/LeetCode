772ff5388db21d1328bc4ea42175c30e8d9e54cf
Deep1345 <pateldeepvi@gmail.com>
2026-08-18 22:22:48 +0530

    ✅ Add 1666. Make The String Great [Easy]
    Folder : Stacks/
    Tags   : String, Stack
    Lang   : Cpp

--- Stacks/1666_Make_The_String_Great.cpp
+++ Stacks/1666_Make_The_String_Great.cpp
@@ -33,9 +33,10 @@ public:
         }
         string ans;
         while(!st.empty()){
-            ans = st.top() + ans;
+            ans += st.top();
             st.pop();
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
