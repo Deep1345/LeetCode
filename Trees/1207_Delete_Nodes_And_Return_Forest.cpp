9bed298078a2025255f8754514911c432f5c22b3
Deep1345 <pateldeepvi@gmail.com>
2026-08-23 21:06:42 +0530

    ✅ Add 1207. Delete Nodes And Return Forest [Medium]
    Folder : Trees/
    Tags   : Array, Hash Table, Tree
    Lang   : Cpp

---
+++ Trees/1207_Delete_Nodes_And_Return_Forest.cpp
@@ -0,0 +1,53 @@
+// Problem   : 1207. Delete Nodes And Return Forest
+// Difficulty: Medium
+// Tags      : Array, Hash Table, Tree, Depth-First Search, Binary Tree
+// URL       : https://leetcode.com/problems/delete-nodes-and-return-forest/
+// Language  : Cpp
+// Date      : 2026-08-23
+//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* del(TreeNode* root, unordered_set<int>& st, vector<TreeNode*>& ans){
+        if(root==NULL){
+            return NULL;
+        }
+        root->left = del(root->left,st,ans);
+        root->right = del(root->right,st,ans);
+        if(st.find(root->val)!=st.end()){
+            if(root->left){
+                ans.push_back(root->left);
+            }
+            if(root->right){
+                ans.push_back(root->right);
+            }
+            return NULL;
+        }
+        else{
+            return root;
+        }
+    }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        unordered_set<int> st;
+        for(auto it: to_delete){
+            st.insert(it);
+        }
+        vector<TreeNode*> ans;
+        del(root,st,ans);
+        if(st.find(root->val)==st.end()){
+            ans.push_back(root);
+        }
+        return ans;
+    }
+};
