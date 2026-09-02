e5763f91ad3f200d2578f2066dae9b30798f9f52
Deep1345 <pateldeepvi@gmail.com>
2026-09-02 23:43:02 +0530

    ✅ Add 127. Word Ladder [Hard]
    Folder : Hash-Tables/
    Tags   : Hash Table, String, Breadth-First Search
    Lang   : Cpp

---
+++ Hash-Tables/0127_Word_Ladder.cpp
@@ -0,0 +1,37 @@
+// Problem   : 127. Word Ladder
+// Difficulty: Hard
+// Tags      : Hash Table, String, Breadth-First Search, Bidirectional Search
+// URL       : https://leetcode.com/problems/word-ladder/
+// Language  : Cpp
+// Date      : 2026-09-02
+//
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        set<string> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!q.empty()){
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            if(word==endWord){
+                return steps;
+            }
+            for(int i=0;i<word.size();i++){
+                char original = word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,steps+1});
+                        st.erase(word);
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+};
