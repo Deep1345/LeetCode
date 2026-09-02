618f69fe996045c8e2547e5b7f5d6d27749cec50
Deep1345 <pateldeepvi@gmail.com>
2026-09-02 08:36:33 +0530

    ✅ Add 4256. Construct Uniform Parity Array I [Easy]
    Folder : Arrays/
    Tags   : Array, Math
    Lang   : Cpp

---
+++ Arrays/4256_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,15 @@
+// Problem   : 4256. Construct Uniform Parity Array I
+// Difficulty: Easy
+// Tags      : Array, Math
+// URL       : https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Language  : Cpp
+// Date      : 2026-09-02
+//
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        //always true
+        return true;
+    }
+};
