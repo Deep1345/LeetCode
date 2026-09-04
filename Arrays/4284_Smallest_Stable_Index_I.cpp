d97faac65a703669d7c8c7c8069b0f60bef43966
Deep1345 <pateldeepvi@gmail.com>
2026-09-04 08:07:33 +0530

    ✅ Add 4284. Smallest Stable Index I [Easy]
    Folder : Arrays/
    Tags   : Array, Prefix Sum
    Lang   : Cpp

--- Arrays/4284_Smallest_Stable_Index_I.cpp
+++ Arrays/4284_Smallest_Stable_Index_I.cpp
@@ -10,16 +10,18 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> suffix(n);
+        vector<int> prefix(n);
+        suffix[n-1] = nums[n-1];
+        prefix[0] = nums[0];
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = min(nums[i],suffix[i+1]);
+        }
+        for(int i=1;i<n;i++){
+            prefix[i] = max(nums[i],prefix[i-1]);
+        }
         for(int i=0;i<n;i++){
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for(int j=0;j<=i;j++){
-                maxi = max(maxi,nums[j]);
-            }
-            for(int k=i;k<n;k++){
-                mini = min(mini,nums[k]);
-            }
-            int diff = maxi-mini;
+            int diff = prefix[i]-suffix[i];
             if(diff<=k){
                 return i;
             }
