777260d2fe39e61f108a728926aeefafa74d359c
Deep1345 <pateldeepvi@gmail.com>
2026-09-09 08:12:50 +0530

    ✅ Add 4248. Count Commas in Range II [Medium]
    Folder : Math/
    Tags   : Math
    Lang   : Cpp

---
+++ Math/4248_Count_Commas_in_Range_II.cpp
@@ -0,0 +1,18 @@
+// Problem   : 4248. Count Commas in Range II
+// Difficulty: Medium
+// Tags      : Math
+// URL       : https://leetcode.com/problems/count-commas-in-range-ii/
+// Language  : Cpp
+// Date      : 2026-09-09
+//
+
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        for (long long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
+        }
+        return ans;
+    }
+};
