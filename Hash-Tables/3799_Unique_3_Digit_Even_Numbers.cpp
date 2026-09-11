8ed5a87bf8676b125cbadc9b681d58f2fb539010
Deep1345 <pateldeepvi@gmail.com>
2026-09-11 16:35:03 +0530

    ✅ Add 3799. Unique 3-Digit Even Numbers [Easy]
    Folder : Hash-Tables/
    Tags   : Array, Hash Table, Recursion
    Lang   : Cpp

---
+++ Hash-Tables/3799_Unique_3_Digit_Even_Numbers.cpp
@@ -0,0 +1,34 @@
+// Problem   : 3799. Unique 3-Digit Even Numbers
+// Difficulty: Easy
+// Tags      : Array, Hash Table, Recursion, Enumeration
+// URL       : https://leetcode.com/problems/unique-3-digit-even-numbers/
+// Language  : Cpp
+// Date      : 2026-09-11
+//
+
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        vector<int> freq(10, 0);
+        for (int digit : digits) {
+            freq[digit]++;
+        }
+        int ans = 0;
+        for (int i = 1; i <= 9; i++) {
+            if (freq[i] == 0) continue;
+            freq[i]--;
+            for (int j = 0; j <= 9; j++) {
+                if (freq[j] == 0) continue;
+                freq[j]--;
+                for (int k = 0; k <= 8; k += 2) {
+                    if (freq[k] > 0) {
+                        ans++;
+                    }
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
+        return ans;
+    }
+};
