02c41e7c26be3dc5180cc653637f33ba1877dfea
Deep1345 <pateldeepvi@gmail.com>
2026-09-14 09:43:03 +0530

    ✅ Add 966. Binary Subarrays With Sum [Medium]
    Folder : Hash-Tables/
    Tags   : Array, Hash Table, Sliding Window
    Lang   : Cpp

--- Hash-Tables/0966_Binary_Subarrays_With_Sum.cpp
+++ Hash-Tables/0966_Binary_Subarrays_With_Sum.cpp
@@ -3,29 +3,30 @@
 // Tags      : Array, Hash Table, Sliding Window, Prefix Sum
 // URL       : https://leetcode.com/problems/binary-subarrays-with-sum/
 // Language  : Cpp
-// Date      : 2026-08-15
+// Date      : 2026-09-14
 //
 
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int ans = no(nums, goal) - no(nums,goal-1);
-        return ans;
+        return find(nums,goal) - find(nums,goal-1);
     }
-    int no(vector<int>& nums, int goal){
+    int find(vector<int>& nums, int goal){
         if(goal<0){
             return 0;
         }
-        int l = 0, r = 0, cnt = 0, sum = 0;
-        while(r<nums.size()){
+        int n = nums.size();
+        int l = 0;
+        int ans = 0;
+        int sum = 0;
+        for(int r=0;r<n;r++){
             sum += nums[r];
             while(sum>goal){
-                sum -= nums[l];
+                sum-=nums[l];
                 l++;
             }
-            cnt += (r-l+1);
-            r++;
+            ans += r-l+1;
         }
-        return cnt;
+        return ans;
     }
 };
