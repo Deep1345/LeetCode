cc2c8a980aea00198c0d13b57553bb309d84e7e1
Deep1345 <pateldeepvi@gmail.com>
2026-09-14 07:42:22 +0530

    ✅ Add 1460. Number of Substrings Containing All Three Characters [Medium]
    Folder : Hash-Tables/
    Tags   : Hash Table, String, Sliding Window
    Lang   : Cpp

--- Hash-Tables/1460_Number_of_Substrings_Containing_All_Three_Characters.cpp
+++ Hash-Tables/1460_Number_of_Substrings_Containing_All_Three_Characters.cpp
@@ -3,21 +3,22 @@
 // Tags      : Hash Table, String, Sliding Window
 // URL       : https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
 // Language  : Cpp
-// Date      : 2026-08-16
+// Date      : 2026-09-14
 //
 
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int cnt[3] = {0};
-        int i = 0;
+        vector<int> arr(3,0);
+        int n = s.size();
+        int l = 0;
         int ans = 0;
-        for (int j = 0; j < s.size(); j++) {
-            cnt[s[j] - 'a']++;
-            while (cnt[0] > 0 && cnt[1] > 0 && cnt[2] > 0) {
-                ans += s.size() - j;
-                cnt[s[i] - 'a']--;
-                i++;
+        for(int r=0;r<n;r++){
+            arr[s[r]-'a']++;
+            while(arr[0]>0 && arr[1]>0 && arr[2]>0){
+                ans += n-r;
+                arr[s[l]-'a']--;
+                l++;
             }
         }
         return ans;
