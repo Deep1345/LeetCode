6bb7ea6f529346b05e1bd2ff04f578816fd300b7
Deep1345 <pateldeepvi@gmail.com>
2026-09-16 07:24:38 +0530

    ✅ Add 35. Search Insert Position [Easy]
    Folder : Binary-Search/
    Tags   : Array, Binary Search
    Lang   : Cpp

--- Binary-Search/0035_Search_Insert_Position.cpp
+++ Binary-Search/0035_Search_Insert_Position.cpp
@@ -3,23 +3,12 @@
 // Tags      : Array, Binary Search
 // URL       : https://leetcode.com/problems/search-insert-position/
 // Language  : Cpp
-// Date      : 2026-08-14
+// Date      : 2026-09-16
 //
 
-class Solution {
+class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size()-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]<target){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return low;
-    }
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    }
 };
