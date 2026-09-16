d96cf07976ea54dc03fa7d436496cd83c7979753
Deep1345 <pateldeepvi@gmail.com>
2026-09-16 08:43:04 +0530

    ✅ Add 33. Search in Rotated Sorted Array [Medium]
    Folder : Binary-Search/
    Tags   : Array, Binary Search
    Lang   : Cpp

---
+++ Binary-Search/0033_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,39 @@
+// Problem   : 33. Search in Rotated Sorted Array
+// Difficulty: Medium
+// Tags      : Array, Binary Search
+// URL       : https://leetcode.com/problems/search-in-rotated-sorted-array/
+// Language  : Cpp
+// Date      : 2026-09-16
+//
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else if(nums[high]>=nums[mid]){
+                if(nums[mid]<target && nums[high]>=target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
